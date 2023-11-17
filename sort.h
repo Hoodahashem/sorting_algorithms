6ff56779450d6934cbc92caf492af69b7c1d8098
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-17 21:03:15 +0200

    again

--- sort.h
+++ sort.h
@@ -29,7 +29,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 int list_size(listint_t **list);
 int list_size(listint_t **list);
-
+void swap(listint_t **head, listint_t **list1, listint_t *list2);
 
 
 
