be47ab9efa2f81b85411bead98edc8bfdcface2d
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-17 21:39:08 +0200

    try 2 on the same function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,25 +7,19 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	int i, j, min, temp;
-
-	for (i = 0; i < size - 1; i++)
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		min = i;
+		size_t min = i;
 
-		for (j = i + 1; j < size; j++)
+		for (size_t j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 			{
-				min = j;
+				int temp = array[j];
+
+				array[j] = array[min];
+				array[min] = temp;
 			}
-		}
-		if (min != i)
-		{
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
 		}
 	}
 }
