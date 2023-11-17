fcf935e844bc347740740e71ec24fefe36965554
Hoodahashem <mahmoudhashem335@gmail.com>
2023-11-17 21:44:00 +0200

    the answer after debugging

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,11 +7,13 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		size_t min = i;
 
-		for (size_t j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 			{
@@ -19,6 +21,7 @@ void selection_sort(int *array, size_t size)
 
 				array[j] = array[min];
 				array[min] = temp;
+				print_array(array, size);
 			}
 		}
 	}
